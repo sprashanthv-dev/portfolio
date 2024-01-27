3bd27e4211e85624ffc9cd3bede21233885b4530
sprashanthv-cu <Prashanthvamanan3@gmail.com>
2024-01-27 00:50:54 -0700

    feat: Profile + WorkEx styles cleanup + removed unnecessary dependencies

--- components/Profile/Profile.tsx
+++ components/Profile/Profile.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 
-import styles from './Profile.module.scss';
-
 const Profile = () => {
   return (
     <>
-      <div className={styles.profile_summary}>
-        <p>
+      <div className="p-2 mx-4 my-0 md:pt-4">
+        <p className="md:text-2xl text-justify text-xl">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad atque
           modi quae saepe vitae. Fugit iure nesciunt quos reprehenderit!
           Aspernatur at dolorem exercitationem facilis laboriosam officia saepe
