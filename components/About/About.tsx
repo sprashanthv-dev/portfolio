18b04a6b28681010296faa3a409de42fbe8ed7c7
sprashanthv-cu <Prashanthvamanan3@gmail.com>
2024-02-03 21:10:43 -0700

    feat: Minor Changes - About and Intro Sections

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -4,13 +4,13 @@ const About = () => {
       <p className="text-center text-xl underline underline-offset-4 decoration-2 md:text-2xl">
         About
       </p>
-      <div className="flex justify-between items-center">
+      <div className="flex flex-col lg:flex-row justify-between items-center">
         <img
           src="images/me_4.jpg"
           alt="Photo of me"
-          className="rounded-full m-3 p-0 md:p-6 w-36 md:w-80"
+          className="rounded-full mt-6 p-0 md:p-6 w-48 md:w-72"
         />
-        <div className="about my-5">
+        <div className="about my-5 md:text-xl">
           <p className="text-justify mb-3">
             I have three years of professional experience as a full-stack
             engineer, designing and developing highly-scalable and
--- components/Intro/Intro.tsx
+++ components/Intro/Intro.tsx
@@ -2,17 +2,21 @@ import React from 'react';
 
 const Profile = () => {
   return (
-    <div>
-      <p className="my-3 text-center md:text-xl">Hi, Nice to meet you! I am</p>
-      <p className="text-2xl text-center md:text-5xl m-0">
-        Prashanth Srinivasan.
-      </p>
-      <p className="my-4 md:text-xl text-justify">
+    <div className="p-20">
+      <p className="my-3 md:text-xl">Hi, Nice to meet you! I am</p>
+      <p className="text-3xl md:text-6xl m-0">Prashanth Srinivasan.</p>
+      <p className="my-4 md:text-2xl text-justify">
         I am a full-stack engineer by trade, specializing in crafting clean,
-        accessible and highly-performant web applications. Currently, I am
-        pursuing a Master's degree in Computer Science with a focus on Cloud
-        Computing and Software Systems at the University of Colorado, Boulder.
+        accessible and highly-performant web applications.
       </p>
+      <p className="my-6 md:text-2xl text-justify">
+        Currently, I am pursuing a Master's degree in Computer Science with a
+        focus on Cloud Computing and Software Systems at the University of
+        Colorado, Boulder.
+      </p>
+      <button className="border-solid border-2 p-2 rounded border-navbar-hover hover:bg-navbar-text hover:text-white">
+        Contact me
+      </button>
     </div>
   );
 };
