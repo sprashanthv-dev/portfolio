2263a3e3c812c5bb492fb03a238e2e24f7e5992e
sprashanthv-cu <Prashanthvamanan3@gmail.com>
2024-03-15 17:08:15 -0600

    feat: Modified next config + configured Google Analytics

--- next.config.js
+++ next.config.js
@@ -1,24 +1,5 @@
-const path = require('path');
-
-require('dotenv').config();
-
 module.exports = {
   basePath: "/portfolio",
   output: "export",
-  env: {
-    API_URL: process.env.API_URL,
-  },
-  compiler: {
-    styledComponents: {
-      ssr: true,
-      displayName: true,
-    },
-  },
-  webpack(config) {
-    config.resolve.alias.pages = path.join(__dirname, 'pages');
-    config.resolve.alias.components = path.join(__dirname, 'components');
-    config.resolve.alias.lib = path.join(__dirname, 'lib');
-    config.resolve.alias.styles = path.join(__dirname, 'styles');
-    return config;
-  },
+  reactStrictMode: true
 };
