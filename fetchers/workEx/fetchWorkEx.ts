7d2336e870f3791ae9eddc44a0cac41d352bfd40
sprashanthv-cu <Prashanthvamanan3@gmail.com>
2024-03-15 17:20:05 -0600

    deploy-fix: Theme Issues + Work info

--- fetchers/workEx/fetchWorkEx.ts
+++ fetchers/workEx/fetchWorkEx.ts
@@ -1,7 +1,7 @@
 // TODO: Now retrieving mock work_ex json. To replace with
 //       api call to database.
 const fetchWorkEx = async () => {
-  const resource = await fetch(`/data/work_ex/work_ex.json`);
+  const resource = await fetch(`data/work_ex/work_ex.json`);
 
   if (!resource.ok) {
     throw new Error(`Failed to fetch work_ex data`);
